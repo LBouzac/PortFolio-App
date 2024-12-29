d9d56cf89aaf1cca15122035a0735c303cb0ec94
LBouzac <bouzacludovic@gmail.com>
2024-12-29 16:19:30 +0100

    maj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
-import { SearchbarComponent } from './components/searchbar/searchbar.component';
-import { AboutMeComponent } from './pages/about-me/about-me.component';
-import { IndexComponent } from './pages/index/index.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {TableComponent} from './table/table.component';
+import {TreeComponent} from './tree/tree.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    SearchbarComponent,
-    IndexComponent,
-    // AboutMeComponent
+    NavigationComponent,
+    DashboardComponent,
+    TableComponent,
+    TreeComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  count: number = 0;
-  $search: String = "";
 
-  increaseCount() {
-    this.count ++;
-  }
 }
